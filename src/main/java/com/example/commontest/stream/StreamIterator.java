b4d9698da8bae50340f67707ce619d4192778799
minsoo <minsoo651@naver.com>
2024-03-17 21:10:21 +0900

    stream의 iterator 메소드 사용 시 List로 형변환 가능한지 테스트 이펙티브 자바에서 스트림 관련해서 보고있는데 List > Collection > Iterable 인터페이스 구조이길래 형변환 되나 확인해봤는데 안됨 다운캐스팅 문제인가?

---
+++ src/main/java/com/example/commontest/stream/StreamIterator.java
@@ -0,0 +1,9 @@
+package com.example.commontest.stream;
+
+public class StreamIterator {
+
+    public void test(){
+
+    }
+
+}
---
+++ src/test/java/com/example/commontest/stream/StreamIteratorTest.java
@@ -0,0 +1,24 @@
+package com.example.commontest.stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class StreamIteratorTest {
+
+    @Test
+    @DisplayName("stream의 iterator을 사용해서 Collection 즉 List로 변환이 가능한지 테스트")
+    public void iteratorToList(){
+
+        List<String> originList = List.of("a", "b", "c");
+
+        Assertions.assertThrows(ClassCastException.class, () -> {
+            List<String> newList = (List<String>) originList.stream()
+                    .iterator();
+        });
+    }
+
+
+}
